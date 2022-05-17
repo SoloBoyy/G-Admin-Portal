284a4ff1e4dd48b70c21122539db72db3d195e42
richiekumar <richiekumar214@gmail.com>
2022-05-17 14:44:02 -0500

    New UI Redesign

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import ReactDOM from "react-dom";
+import { FaEnvelope } from "react-icons/fa";
+import { FaSearchLocation } from "react-icons/fa";
+import { FaPhone} from "react-icons/fa";
 
 import "./App.css";
 import "./styles.css"
@@ -103,85 +106,55 @@ const App = () => {
 
   return (
 <Fragment>
-    <div className="app">
+      
       <div className="login-form">
+        
         <div className="title">{titleText}</div>
         {isSubmitted ? 
         <section>
           {apiData.map((merchant) => {
                             return (
-                                <div className="movie-container" key={String(merchant.id)}>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/4.9.95/css/materialdesignicons.css" rel="stylesheet"/>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4=" crossorigin="anonymous" />
-<div class="container">
-        <div class="col-lg-9">
-            <div class="candidates-listing-item">
-                <div class="list-grid-item mt-4 p-2">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <div class="candidates-img float-left mr-4" className='together'>
-                                <img src={merchant.imgurl} alt="" class="img-fluid d-block rounded" className='img'/>
-                                <h5 class="mb-2 f-19"><a class="text-dark" className='togetherTwo'>{merchant.name}</a></h5>
-                            </div>
-                            <div class="candidates-list-desc job-single-meta  pt-2" className='containerInfo'>
-                                
-                                <ul class="list-inline mb-0">
-                                  <div className='set'>
-                                    <li class="list-inline-item mr-4">
-                                        <p class="text-muted f-15 mb-0"><i class="mdi mdi-account mr-1"></i>{merchant.role}</p>
-                                    </li>
-
-                                    <li class="list-inline-item mr-4" className='from'>
-                                        <p class="f-15 mb-0"><a class="text-muted"><i class="mdi mdi-map-marker mr-1"></i>{merchant.city}, {merchant.state}</a></p>
-                                    </li>
-                                    </div>
-                                    <li class="list-inline-item">
-                                        <p class="text-muted f-15 mb-0" className='hours'><i class="mdi mdi-currency-usd mr-1"></i>{merchant.hourly}/hour</p>
-                                    </li>
+                              <div class="card-container">
+                              <span class="pro">{merchant.role}: ${merchant.hourly} / hour </span>
+                              
+                              <img class="round" src= {merchant.imgurl} alt="user" />
+                              <h3>{merchant.name} {merchant.lname}</h3>
+                             
+                            
+                              <h6><FaSearchLocation />&nbsp; {merchant.city}, {merchant.state}</h6>
+                              <h6><FaEnvelope />&nbsp;  {merchant.email}</h6>
+                              <h6><FaPhone /> &nbsp; {merchant.number}</h6>
+                               <p>{merchant.bio}</p>
+                              <div class="buttons">
+                               
+                                <form action={merchant.resume} method="get" target="_blank">
+                                <button class="primary">
+                                  Set Meeting
+                                </button>
+                                  &nbsp;&nbsp;
+                                <button class="primary ghost">
+                                  Resume
+                                </button>
+                                </form>
+                              </div>
+                              <div class="skills">
+                                <h6>Skills</h6>
+                                <ul>
+                                  <li>{merchant.skills}</li>
                                 </ul>
-                                <p class="text-muted mt-1 mb-0">Skills: {merchant.skills}</p>
-                                </div>
+                              </div>
+                              <div>
+                              <hr></hr>
                             </div>
-                        </div>
-
-                        <div class="col-md-3" className='bottom'>
-                            <div class="candidates-list-fav-btn text-right">
-                                <div class="fav-icon" className='heart'>
-                                    <i class="mdi mdi-heart f-20"></i>
-                                </div>
-                                <div class="candidates-listing-btn mt-4" className='Hire'>
-                                    <a href="#" class="btn btn-outline btn-sm" ><button type="button" class="btn btn-danger">Hire Applicant</button></a>
-                                    
-                                </div>
-                                
                             </div>
                             
-                        </div>
-                        <hr></hr>
-                    
-
-                    <div class="row">
-                        <div class="col-lg-11 offset-lg-1">
-                            <div class="candidates-item-desc" className='texter'>
-                               
-                                <p class="text-muted mb-2 f-14">{merchant.bio}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-              </div>
-              </div>
-              </div>
                             );
                         })}
         </section> : renderForm}
         
     
       </div>
-    </div>
-    
+   
     </Fragment>
   );
 }
